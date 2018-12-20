781be554fd65a69b4d1378dce4c9b26879a9cbb6
Bùi Thị Ngọc Anh <ngocanhbk53@gmail.com>
2018-12-20 16:04:57 +0700

    commit general

---
+++ SaleManagement/Controllers/GeneralController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaleManagement.Controllers
+{
+    public class GeneralController : Controller
+    {
+        // GET: General
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View("~/Views/admin_1_rounded/page_general_about.cshtml");
+        }
+
+        public ActionResult Contact()
+        {
+            return View("~/Views/admin_1_rounded/page_general_contact.cshtml");
+        }
+        public ActionResult Pricing()
+        {
+            return View("~/Views/admin_1_rounded/page_general_pricing.cshtml");
+        }
+    }
+}
